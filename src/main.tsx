93c7a076865b98842a7e47112d2d4434896d9f7d
asadulloev <asadulloev@deck.lc>
2024-07-02 19:02:16 +0300

    change token provider

--- src/main.tsx
+++ src/main.tsx
@@ -14,6 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <ConfigProvider
         theme={{
           token: {
+            colorPrimary: '#ff725e',
             fontFamily: 'Montserrat',
           },
         }}>
